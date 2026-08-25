c2098fd805a2efc7db9b5eac0f885517f958299b
jordigarciamorilla86 <jordigm86@gmail.com>
2026-08-25 10:37:58 +0200

    fix: allow agenda scroll chaining

--- app/business-dashboard/agenda/components/AgendaGrid.tsx
+++ app/business-dashboard/agenda/components/AgendaGrid.tsx
@@ -326,7 +326,7 @@ export default function AgendaGrid({
             "contain",
 
           overscrollBehaviorY:
-            "contain",
+            "auto",
 
           opacity:
             loadingWeek
